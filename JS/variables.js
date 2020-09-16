fa2abb82e23a90e3b1091e2ab42dcd1db926d1be
Ervin MG <ervinjoel2323@gmail.com>
2020-09-16 00:22:48 -0600

    aprendiendo mas _js

--- JS/variables.js
+++ JS/variables.js
@@ -3,5 +3,9 @@ var SegundoNombre = 'Alejandro';
 var apellidos = 'Mikaelson obres';
 var edad = 20;
 
+var caja; /* usando variables como caja, sin definir su valor, luego poemos meter el valor */
+caja = 'capacidad';
+
 console.log(apellidos);
 console.log(edad);
+console.log(caja);
