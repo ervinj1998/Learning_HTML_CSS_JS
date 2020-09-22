b7e83ac5ca8b41d8a9a7ff6d713ad7350b305ac2
Ervin MG <ervinjoel2323@gmail.com>
2020-09-22 12:51:27 -0600

    practicando js variables y arreglos

--- JS/variables.js
+++ JS/variables.js
@@ -9,3 +9,19 @@ caja = 'capacidad';
 console.log(apellidos);
 console.log(edad);
 console.log(caja);
+
+/*Pueba para imporimir y hacer calculos en javascript */
+/*alert('ERROR, no se puede cargar la pagina.')  para mostrar mensaje de alerta en la pagina*/ 
+
+var num1 =5
+var num2 =4
+
+var arreglo = ['Enero',' Febrero',' Marzo',' Abril',' Mayo',' Junio'];
+
+respuesta=(num1+num2)
+document.write(num1,'+',num2,'=',(num1+num2))
+document.write('<br/>',respuesta,'<br/>')    /* <br/>  es par ahacer un salto de linea.*/
+document.write(PrimerNombre,' ',SegundoNombre)
+document.write('<br/>',arreglo[5]);
+document.write('<br/>',arreglo);
+console.log(arreglo[5]);
